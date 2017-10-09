9a7d143526c767c4c1b880741dd6f657a602f11f
tzungtzu <needy_wei@hotmail.com>
2017-10-09 09:59:39 +0800

    add web

--- my-web/server.js
+++ my-web/server.js
@@ -1,13 +1,14 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route,handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for "+ pathname +" received.");
-		route(pathname);
+		
 		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello World");
+		var content = route(handle, pathname);
+		response.write(content);
 		response.end();
 	}
 
@@ -16,3 +17,4 @@ function start(route) {
 }
 
 exports.start = start;
+
